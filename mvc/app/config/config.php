be5e1addf4cb33831b998f9af457bf67102dbf9f
Fadhillanr <121225974+Fadhillanr@users.noreply.github.com>
2024-10-30 18:06:22 +0700

    Add files via upload

---
+++ mvc/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2024_a_223040082_pw/mvc/public');
+
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', '');
+define ('DB_NAME','phpmvc');
---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+
+    <h1>About me</h1>
+    <h1 class="mt-4" >About me</h1>
+    <img src="<?= BASEURL;?>/img/fadhilla-profile.jpg" alt="Fadhilla Nur Islami" width="200" class="rounded-circle shadow" >
+    <p>Hallo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
+
+</div>
